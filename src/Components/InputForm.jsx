4f443ccd7361f7f230e195cfd6a1a11d72b2a5d2
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-24 14:26:38 +0530

    removed useEffect warning

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 const InputForm = ({ addNewTodo, singleTodo, setEditTodo }) => {
